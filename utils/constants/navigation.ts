8cf5e22220d0c70eb8ee665318fe1c02f580fec1
fabiconcept <favourajokubi@gmail.com>
2025-08-22 14:54:14 +0100

    Make Updates from corrections

--- utils/constants/navigation.ts
+++ utils/constants/navigation.ts
@@ -44,7 +44,7 @@ export const NAV_LINKS: NavLink[] = [
 
 export const CONTACT_INFO = {
   address: "State Secretariat Complex, Port Harcourt Road, Owerri, Imo State, Nigeria",
-  email: "info@imolabour.ng",
+  email: "commissioner.info@labour.im.gov",
   phone: "+234 803 000 1111",
   hours: "Monday - Friday, 8:00 AM – 4:00 PM (WAT)"
 };
